57b90d25d47db59025d2fe8425a1abb0f730215d
rifkisubhanarifin-art <rifkisubhanarifin@gmail.com>
2026-05-19 12:08:38 +0700

    Simpan file JadwalPage

--- src/Pages/JadwalPage.jsx
+++ src/Pages/JadwalPage.jsx
@@ -12,7 +12,7 @@ export const JadwalPage = () => {
     const [formData, setFormData] = useState({
         hari: "",
         waktu: "",
-        mataPelajaran: "",
+        mataPelajaran: "", 
         kelas: ""
     });
 
